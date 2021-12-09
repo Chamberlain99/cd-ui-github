cc7ead14280f262cff27422c343f0cf4b746f28a
chenchucheng <chenchucheng@zeen-cn.com>
2021-12-09 23:52:00 +0800

    honor pc test

--- examples/components/HelloWorld.vue
+++ examples/components/HelloWorld.vue
@@ -3,7 +3,7 @@
     <img src="../assets/images/homepagePic.png" alt="" />
     <div class="homepage_title">面向个人开发者vue-UI组件库</div>
     <div class="navBox">
-      <div class="navBox1" @click="startUse">开始使用</div>
+      <div class="navBox1" @click="startUse">开始使用2</div>
       <div class="navBox2">设计体系1</div>
     </div>
   </div>
